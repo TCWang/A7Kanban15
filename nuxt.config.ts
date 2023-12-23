56c7119cf6f454e9bb9a1c65705bfb53490c1242
TC Wang <wangtct@hotmail.com>
2023-12-24 07:47:26 +0800

    system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxt/image-edge", "@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+  css: ["@/assets/css/tailwind.css"],
+
+  app: {
+    head: {
+      title: "A7社區網站-城市探索",
+      meta: [
+        {
+          name: "description",
+          content: "A7新林口-社區網站 V2.0 15 城市探索",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
+    },
+  },
+});
