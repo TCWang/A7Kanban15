09073c69d4d0f543cb7ee0d1bfcb3dfb31b80e3d
TC Wang <wangtct@hotmail.com>
2024-03-09 06:09:12 +0800

    web

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/image-edge", "@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/image-edge", "@vueuse/nuxt", "@nuxtjs/tailwindcss", "@nuxt/image"],
   css: ["@/assets/css/tailwind.css"],
 
   plugins: [
