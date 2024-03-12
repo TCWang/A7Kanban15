a2ed6c015c11a7f7da68b8a5624b6d99b9018417
TC Wang <wangtct@hotmail.com>
2024-03-12 10:12:42 +0800

    chart test

--- components/charts/Bar01.vue
+++ components/charts/Bar01.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-4">
-    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+    <Bar id="myChart" :options="chartOptions" :data="chartData"></Bar>
   </div>
 </template>
 
@@ -26,16 +26,44 @@ ChartJS.register(
 );
 
 export default {
-  name: "BarChart",
+  name: "BarChartTeamplate",
   components: { Bar },
   data() {
     return {
       chartData: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 63, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 63, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
       },
       chartOptions: {
         responsive: true,
+        indexAxis: "x",
+        scales: {
+          y: {
+            beginAtZero: false,
+          },
+        },
       },
     };
   },
